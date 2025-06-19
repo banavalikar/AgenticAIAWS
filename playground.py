06abf6ba4d02ff9da6d556557305cd4c8d3e03b7
banavalikar <banavalikar@users.noreply.github.com>
2025-06-19 12:36:01 +0100

    Fix for Undefined variable

--- playground.py
+++ playground.py
@@ -1,5 +1,8 @@
-if not numbers:
+import sys
+
+if len(sys.argv) < 2:
     print("Error: No numbers provided.")
 else:
+    numbers = [int(num) for num in sys.argv[1:]]
     for num in numbers:
         print(f"Number: {num}")
