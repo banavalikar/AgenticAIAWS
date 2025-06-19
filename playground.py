6f9555e525744b92ddc06624c5f391614ea88038
banavalikar <banavalikar@users.noreply.github.com>
2025-06-19 12:35:40 +0100

    Fix for Incorrect usage of range()

--- playground.py
+++ playground.py
@@ -1,9 +1,2 @@
-def print_numbers():
-    numbers = [1, 2, 3, 4, 5]
-    
-    for i in range(numbers):  # ❌ Error: 'range' requires an integer, not a list
-        print(f"Number: {i}")
-        return
-
-print_numbers()
-#attempt9
+for i in range(len(numbers)):
+    print(f"Number: {numbers[i]}")
