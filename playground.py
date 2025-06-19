08e1beb51bdbb789dcce4aacf2e35e4675aa08eb
banavalikar <banavalikar@users.noreply.github.com>
2025-06-19 12:39:30 +0100

    Fix for The range() function expects an integer argument, not a list.

--- playground.py
+++ playground.py
@@ -1,9 +1,1 @@
-def print_numbers():
-    numbers = [1, 2, 3, 4, 5]
-    
-    for i in range(numbers):  # ❌ Error: 'range' requires an integer, not a list
-        print(f"Number: {i}")
-        return
-
-print_numbers()
-#attempt10
+for i in range(len(numbers)):
