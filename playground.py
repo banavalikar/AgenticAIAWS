3bf0646cc1112352206c423eddc45600d5b18dd2
banavalikar <banavalikar@users.noreply.github.com>
2025-06-20 08:53:37 +0100

    Fix for Type error

--- playground.py
+++ playground.py
@@ -1,9 +1,7 @@
 def print_numbers():
     numbers = [1, 2, 3, 4, 5]
-    
-    for i in range(numbers):  
-        print(f"Number: {i}")
-        return
 
-print_numbers(6)
-#attempt19
+    for i in numbers:
+        print(f"Number: {i}")
+
+print_numbers()
