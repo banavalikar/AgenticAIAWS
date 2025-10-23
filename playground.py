69462d5135312be049e999598698c35c2feefcc1
banavalikar <banavalikar@users.noreply.github.com>
2025-10-23 13:22:55 +0100

    Update playground.py

--- playground.py
+++ playground.py
@@ -1,4 +1,4 @@
 def add_nums(a, b):
-    sum = a + c        # ❌ Bug 1: 'c' is undefined (NameError)
+    sum = a + c + ox       # ❌ Bug 1: 'c' is undefined (NameError)
     print("Sum is" + sum)  # ❌ Bug 2: trying to concatenate str + int
     return a - b       # ❌ Bug 3: wrong operation (should be addition)
