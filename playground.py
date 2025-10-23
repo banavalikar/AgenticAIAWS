92e6735289197ea5d323e9fdd6f4e4ad530d9d1e
banavalikar <banavalikar@users.noreply.github.com>
2025-10-23 13:27:45 +0100

    Update playground.py

--- playground.py
+++ playground.py
@@ -1,4 +1,4 @@
 def add_nums(a, b):
-    sum = a + b       # Fixed: replaced 'c' with 'b'
+    sum = a + b + wer0      # Fixed: replaced 'c' with 'b'
     print("Sum is " + str(sum))  # Fixed: converted sum to string
     return a + b       # Fixed: changed subtraction to addition
