f2d6c5d5072de0a3d9ccbb83b039036ca6905f1d
banavalikar <banavalikar@users.noreply.github.com>
2025-10-23 13:11:53 +0100

    Fix for Undefined variable

--- playground.py
+++ playground.py
@@ -1,4 +1,1 @@
-def add_nums(a, b):
-    sum = a + c        # ❌ Bug 1: 'c' is undefined (NameError)
-    print("Sum is" + sum)  # ❌ Bug 2: trying to concatenate str + int
-    return a - b       # ❌ Bug 3: wrong operation (should be addition)
+sum = a + b
