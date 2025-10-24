0df643083aefd64315cd15a19c0c43812e8e4994
banavalikar <banavalikar@users.noreply.github.com>
2025-10-24 08:14:19 +0100

    Fix for Lack of input validation

--- playground.py
+++ playground.py
@@ -1,4 +1,5 @@
 def add_nums(a, b):
+    if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
+        raise ValueError("Input values must be numbers.")
     result = a + b
-    print("Sum is {}".format(result))
     return result
